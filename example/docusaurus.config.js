91b148fc2b73217b8faa8da3990c7d00a3b10c80
Arthur Lenoir <lenoir.arthur@gmail.com>
2022-05-22 10:57:46 +0200

    fix: config for deployment

--- example/docusaurus.config.js
+++ example/docusaurus.config.js
@@ -6,25 +6,25 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'NUPES',
+  tagline: 'Example website based on docusaurus-theme-nupes and nupes-ui design system',
+  url: 'https://arthurlenoir.github.io',
+  baseUrl: '/docusaurus-theme-nupes/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'arthurlenoir', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-theme-nupes', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'fr',
+    locales: ['fr'],
   },
 
   presets: [
